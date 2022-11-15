ce4add27911b6c8597985029f13b2ba09f985dac
a-nemoto313 <a-nemoto@saitoh.ecnet.jp>
2022-11-15 18:20:12 +0900

    Initial commit

---
+++ my_package/__init__.py
@@ -0,0 +1,2 @@
+from .my_module import *
+from .__version__ import __version__
---
+++ my_package/my_module.py
@@ -0,0 +1,6 @@
+class MyClass:
+    def __init__(self):
+        print("MyClass object is created.")
+
+    def my_func(self):
+        print("hello, world.")

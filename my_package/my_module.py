941069bd2626425165390519deafd7e7d0f56292
a-nemoto313 <a-nemoto@saitoh.ecnet.jp>
2022-11-15 18:32:04 +0900

    バージョン番号を答える関数に変更

--- my_package/my_module.py
+++ my_package/my_module.py
@@ -1,6 +1,9 @@
+from __version__ import __version__
+
+
 class MyClass:
     def __init__(self):
         print("MyClass object is created.")
 
     def my_func(self):
-        print("hello, world.")
+        print(f"This library is {__version__}")

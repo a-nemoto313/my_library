83da77e8fbbfff2ee0df53d7728336b6bc4766d8
a-nemoto313 <a-nemoto@saitoh.ecnet.jp>
2022-12-15 14:26:28 +0900

    ドキュメント追加

--- my_package/__init__.py
+++
@@ -1,2 +0,0 @@
-from .my_module import *
-from .__version__ import __version__
--- my_package/my_module.py
+++
@@ -1,6 +0,0 @@
-class MyClass:
-    def __init__(self):
-        print("MyClass object is created.")
-
-    def my_func(self):
-        print("hello, world.")
